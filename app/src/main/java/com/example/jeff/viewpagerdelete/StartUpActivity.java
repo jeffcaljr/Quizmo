e4d6f1342a19c5ca684b26c9fffe37dba3df3f02
jamyspencer <jamy_spencer@hotmail.com>
2017-03-10 18:05:35 -0600

    updated manifest to run startup activity

--- app/src/main/java/com/example/jeff/viewpagerdelete/StartUpActivity.java
+++ app/src/main/java/com/example/jeff/viewpagerdelete/StartUpActivity.java
@@ -1,18 +1,32 @@
 package com.example.jeff.viewpagerdelete;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class StartUpActivity extends AppCompatActivity {
+
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_up);
+
+        loginButton = (Button) findViewById(R.id.loginSubmit);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                authenticate();
+            }
+        });
     }
 
     private void authenticate(){
-
+        Intent quizMe = new Intent(this, QuizLoaderActivity.class);
+        startActivity(quizMe);
     }
 }
 
