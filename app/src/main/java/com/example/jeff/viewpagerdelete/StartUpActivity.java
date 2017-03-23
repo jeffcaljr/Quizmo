a4cd2b6f1c882ed70566d9e261973a35c3143c74
jamyspencer <jamy_spencer@hotmail.com>
2017-03-22 21:48:07 -0500

    changed startup activity to fragment holder, started adding sqlite persistance of username/screenname, created application wide button style.

--- app/src/main/java/com/example/jeff/viewpagerdelete/StartUpActivity.java
+++ app/src/main/java/com/example/jeff/viewpagerdelete/StartUpActivity.java
@@ -1,30 +1,34 @@
 package com.example.jeff.viewpagerdelete;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.ListFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 
-public class StartUpActivity extends AppCompatActivity {
-
-    private Button loginButton;
+public class StartUpActivity extends AppCompatActivity implements LoginFragment.LoginClickListener{
+    Fragment mLoginFragment;
+    FragmentTransaction fragTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_up);
 
-        loginButton = (Button) findViewById(R.id.loginSubmit);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                authenticate();
-            }
-        });
+        mLoginFragment = new LoginFragment();
+        fragTransaction = getSupportFragmentManager().beginTransaction();
+        fragTransaction.add(R.id.startup_container, mLoginFragment);
+        fragTransaction.commit();
+
     }
 
-    private void authenticate(){
+
+    @Override
+    public void onLoginClick() {
         Intent quizMe = new Intent(this, QuizLoaderActivity.class);
         startActivity(quizMe);
         finish();
