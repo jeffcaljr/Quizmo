1fa1324e737db3c5cd65c4485f167ad23f845c5f
Joshua Veal-Briscoe <jkv2c9@mail.umsl.edu>
2017-03-29 16:44:38 -0500

    Added group quiz start page.

--- app/src/main/java/com/example/jeff/viewpagerdelete/GroupQuiz/ActivityControllers/GroupQuizCodeActivity.java
+++ app/src/main/java/com/example/jeff/viewpagerdelete/GroupQuiz/ActivityControllers/GroupQuizCodeActivity.java
@@ -1,69 +1,17 @@
 package com.example.jeff.viewpagerdelete.GroupQuiz.ActivityControllers;
 
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v4.app.Fragment;
 
-import com.android.volley.VolleyError;
-import com.example.jeff.viewpagerdelete.GroupQuiz.Model.Group;
-import com.example.jeff.viewpagerdelete.GroupQuiz.Networking.GroupFetcher;
 import com.example.jeff.viewpagerdelete.GroupQuiz.View.GroupQuizCodeFragment;
-import com.example.jeff.viewpagerdelete.R;
+import com.example.jeff.viewpagerdelete.SingleFragmentActivity;
 
-import java.util.ArrayList;
+/**
+ * Created by Joshua on 3/24/2017.
+ */
 
-public class GroupQuizCodeActivity extends AppCompatActivity implements GroupFetcher.GroupFetcherListener {
-
-    public static final String ARG_GROUP = "ARG_GROUP";
-    public static final String FRAG_TAG_GROUP_QUIZ_CODE_FRAGMENT = "FRAG_TAG_GROUP_QUIZ_CODE_FRAGMENT";
-
-    private FragmentManager manager;
-    private GroupQuizCodeFragment groupQuizCodeFragment;
-
+public class GroupQuizCodeActivity extends SingleFragmentActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_group_quiz_code);
-
-        manager = getSupportFragmentManager();
-
-        GroupFetcher.sharedInstance(this).downloadGroupForUser(this, "jcd39");
-
-
-    }
-
-    @Override
-    public void onDownloadAllGroupsSuccess(ArrayList<Group> groups) {
-
-    }
-
-    @Override
-    public void onDownloadSingleGroupSuccess(Group group) {
-
-        groupQuizCodeFragment = (GroupQuizCodeFragment) manager.findFragmentByTag(FRAG_TAG_GROUP_QUIZ_CODE_FRAGMENT);
-
-        if(groupQuizCodeFragment == null){
-            groupQuizCodeFragment = new GroupQuizCodeFragment();
-            Bundle args = new Bundle();
-            args.putSerializable(ARG_GROUP, group);
-
-            groupQuizCodeFragment.setArguments(args);
-
-            manager.beginTransaction()
-                    .add(R.id.group_code_fragment_container, groupQuizCodeFragment, FRAG_TAG_GROUP_QUIZ_CODE_FRAGMENT)
-                    .commit();
-
-        }
-
-    }
-
-    @Override
-    public void onDownloadAllGroupsFailure(VolleyError error) {
-
-    }
-
-    @Override
-    public void onDownloadSingleGroupFailure(VolleyError error) {
-
+    protected Fragment createFragment() {
+        return new GroupQuizCodeFragment();
     }
 }
