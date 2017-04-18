952a45293beb12d6ad04eb930f8e22d5a140db36
Jeffery Calhoun <jeffcaljr@gmail.com>
2017-04-17 21:40:13 -0500

    Added splash page; havent integrated it yet

---
+++ app/src/main/java/com/example/jeff/viewpagerdelete/Startup/ActivityControllers/Splash.java
@@ -0,0 +1,15 @@
+package com.example.jeff.viewpagerdelete.Startup.ActivityControllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jeff.viewpagerdelete.R;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
