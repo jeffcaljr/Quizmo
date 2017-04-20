a6c372dc1a6605ecabb62bfab22b07f42c02ab11
Jeffery Calhoun <jeffcaljr@gmail.com>
2017-04-19 23:55:35 -0500

    Fixed issue where, if user was on last question in individual quiz and the "unfinished questions" snackbar was shown and dismissed; the "submit quiz" snackbar wouldn't appear unless the user changed questions and came back

--- app/src/main/java/com/example/jeff/viewpagerdelete/IndividualQuiz/Controller/IndividualQuizActivity.java
+++ app/src/main/java/com/example/jeff/viewpagerdelete/IndividualQuiz/Controller/IndividualQuizActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.Snackbar.Callback;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -250,6 +251,13 @@ public class IndividualQuizActivity extends AppCompatActivity
                 });
 
                 snackbar.show();
+              snackbar.setCallback(new Callback() {
+                @Override
+                public void onDismissed(Snackbar snackbar, int event) {
+                  super.onDismissed(snackbar, event);
+                  submitSnackBar.show();
+                }
+              });
 
             }
 
